0e9e48cec8117708f0b26b2cdab971406213a6af
Sören Oldag <soeren_oldag@freenet.de>
2016-05-13 13:38:50 +0200

    Added scripts for crawling events and venues and merging them.

---
+++ scripts/events/crawl_events.py
@@ -0,0 +1,54 @@
+# Crawls event data from http://www.nyc.gov/events for the given time span
+
+import sys
+import math
+import json
+import time
+import requests
+from dateutil import parser
+
+# API endpoint
+url_format = "http://www1.nyc.gov/calendar/api/json/search.htm?&sort=DATE&startDate={start_date}&endDate={end_date}&pageNumber={page}"
+
+# Check number of arguments
+if len(sys.argv) != 4:
+  print("Usage: %s <start_date> <end_date> <output_file>" % sys.argv[0])
+  sys.exit(1)
+
+# Parse arguments
+try:
+  start_date = parser.parse(sys.argv[1]).strftime("%m/%d/%Y %I:%M %p")
+  end_date = parser.parse(sys.argv[2]).strftime("%m/%d/%Y %I:%M %p")
+except ValueError:
+  print("Invalid date format")
+  sys.exit(1)
+output_file = sys.argv[3]
+
+# Crawl event data page by page
+current_page = 1
+last_page = math.inf
+events = []
+tries = 1
+while current_page <= last_page:
+  sys.stdout.write("\rCrawling event data ({0}/{1} pages)".format(current_page, last_page))
+
+  url = url_format.format(start_date=start_date, end_date=end_date, page=current_page)
+  response = requests.get(url)
+  if not response.ok:
+    if tries >= 10:
+      print(" => skip page because of http error {0}".format(response.status_code))
+      tries = 0
+      current_page += 1
+    else:
+      time.sleep(1)
+    tries += 1
+    continue
+
+  response_obj = json.loads(response.text)
+  events += response_obj['items']
+  last_page = response_obj["pagination"]["numPages"]
+  current_page += 1
+
+# Write event data to file
+with open(output_file, 'w') as file:
+  file.write(json.dumps(events))
---
+++ scripts/events/merge_events_venues.py
@@ -0,0 +1,117 @@
+# Merges crawled event and venue data.
+
+import sys
+import math
+import json
+import time
+import requests
+import numpy as np
+from scipy.spatial import cKDTree
+from geopy.distance import vincenty
+
+# Google Maps geocoding API endpoint
+url = "https://maps.googleapis.com/maps/api/geocode/json?address={0}"
+
+geocode_requests_count = 0
+def geocode(query, retries=0):
+  global geocode_requests_count
+
+  response = requests.get(url.format(query))
+
+  geocode_requests_count += 1
+  if geocode_requests_count % 10 == 0:
+    time.sleep(1)
+
+  if response.ok:
+    response = json.loads(response.text)
+    if "results" in response and response["results"]:
+      return response["results"][0]["geometry"]["location"]
+    elif "error_message" in response:
+      if retries < 3:
+        sys.stdout.write("You have exceeded your daily request quota. Renew your ip and press enter.")
+        sys.stdin.read(1)
+        return geocode(query, retries + 1)
+  else:
+    print("\nError {} while geocoding!".format(response.status_code))
+
+
+def isfloat(value):
+  try:
+    float(value)
+    return True
+  except ValueError:
+    return False
+
+
+# Check number of arguments
+if len(sys.argv) != 4:
+  print("Usage: %s <event_file> <venue_file> <output_file>" % sys.argv[0])
+  sys.exit(1)
+
+# Parse arguments
+event_file = sys.argv[1]
+venue_file = sys.argv[2]
+output_file = sys.argv[3]
+
+# Load files as json
+print("Load events from file...")
+with open(event_file, "r") as file:
+  events = json.load(file)
+
+print("Load venues from file...")
+with open(venue_file, "r") as file:
+  venues = json.load(file)
+
+# Group events by address
+grouped_events = {}
+for event in events:
+  if ("geometry" not in event or not event["geometry"]) and "address" in event:
+    address = event["address"]
+    if address in grouped_events:
+      grouped_events[address].append(event)
+    else:
+      grouped_events[address] = [event]
+
+
+# Add missing coordinates to events
+progress_count = 0
+for address, event_group in grouped_events.items():
+  progress_count += 1
+  sys.stdout.write("\rGeocode addresses ({0}/{1})...".format(progress_count, len(grouped_events)))
+
+  location = geocode(address)
+  if location:
+    for event in event_group:
+      event["geometry"] = [location]
+print()
+
+# Group events by coordinates
+grouped_events = {}
+for event in events:
+  if "geometry" in event and event["geometry"]:
+    coords = (event["geometry"][0]["lat"], event["geometry"][0]["lng"])
+    if coords in grouped_events:
+      grouped_events[coords].append(event)
+    else:
+      grouped_events[coords] = [event]
+
+# Map events with coordinates to venues
+progress_count = 0
+coordinate_venues = [venue for venue in venues if "location" in venue and venue["location"]]
+coordinates = np.array([(venue["location"]["lat"], venue["location"]["lon"]) for venue in coordinate_venues])
+tree = cKDTree(coordinates)
+for event_coords, event_group in grouped_events.items():
+  progress_count += 1
+  sys.stdout.write("\rMap events to nearest venue ({0}/{1})...".format(progress_count, len(grouped_events)))
+
+  if isfloat(event_coords[0]) and isfloat(event_coords[1]):
+    min_distance, venue_index = tree.query(event_coords)
+    if vincenty(event_coords, coordinates[venue_index]).meters < 50:
+      for event in event_group:
+        event["venue"] = coordinate_venues[venue_index]
+print()
+
+# Write results to file
+print("Write results to file...")
+with open(output_file, "w") as file:
+  json.dump(events, file)
