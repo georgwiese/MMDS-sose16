e96ba557170ee03a304419335b59e5b8c95a52b4
Georg Wiese <georgwiese@gmail.com>
2016-06-16 16:42:58 +0200

    Update linear regression script to not collect all data

--- spark-scripts/training/linear_regression.py
+++ spark-scripts/training/linear_regression.py
@@ -4,12 +4,13 @@ from pyspark import SparkContext
 from pyspark import SparkConf
 from pyspark.sql import SQLContext
 from pyspark.mllib.regression import LabeledPoint, LinearRegressionWithSGD
+from datetime import datetime
 
 
 # Get file paths from arguments
 if len(sys.argv) != 3:
-    print "Usage: linear_regression.py FEATURES_FILE MODEL_FOLDER"
-    sys.exit()
+  print "Usage: linear_regression.py FEATURES_FILE MODEL_FOLDER"
+  sys.exit()
 features_file = sys.argv[1]
 model_folder = sys.argv[2]
 
@@ -21,20 +22,29 @@ sql_context = SQLContext(sc)
 # Read feature dataframe
 features_df = sql_context.read.parquet(features_file)
 
+# Split into train and test data
+split_date = datetime(2015, 1, 1)
+train_data = features_df.filter(features_df["Time"] < split_date)
+valid_data = features_df.filter(features_df["Time"] > split_date)
+
 # Create feature vector for each district
 def create_point(row):
-    feature_dict = row.asDict()
-    for column in ['Time', 'Lat', 'Lon', 'Pickup_Count']:
-        del feature_dict[column]
+  feature_dict = row.asDict()
+  for column in ['Time', 'Lat', 'Lon', 'Pickup_Count', 'Date']:
+    del feature_dict[column]
 
-    return LabeledPoint(row.Pickup_Count, list(feature_dict.values()))
+  return LabeledPoint(row.Pickup_Count, list(feature_dict.values()))
 
-points_per_district = features_df.map(lambda row: ((row.Lat, row.Lon), create_point(row))).groupByKey()
+def get_data_for_district(df, district):
+  lat, lon = district
+  return df[(df["Lat"] == lat) & (df["Lon"] == lon)]
+
+districts = features_df.select("Lat", "Lon").distinct() \
+            .map(lambda row: (row.Lat, row.Lon)).collect()
 
 # Train model per district and save model
-for district_points in points_per_district.collect():
-	coords = district_points[0]
-	points = district_points[1]
+for lat, lon in districts:
+  points = get_data_for_district(train_data, (lat, lon)).map(create_point)
 
-	model = LinearRegressionWithSGD.train(sc.parallelize(points), iterations=100, step=0.00000001)
-	model.save(sc, '%s/model_%s_%s' % (model_folder, str(coords[0]), str(coords[1])))
+  model = LinearRegressionWithSGD.train(points, iterations=100, step=0.00000001)
+  model.save(sc, '%s/model_%s_%s' % (model_folder, str(lat), str(lon)))
