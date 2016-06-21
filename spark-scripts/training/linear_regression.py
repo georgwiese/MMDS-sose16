f23ebf632355e5501212956134c64537ff34dc63
Georg Wiese <georgwiese@gmail.com>
2016-06-21 19:48:57 +0200

    Refactor linear_regression script, extract AbstractDistrictTrainer class

--- spark-scripts/training/linear_regression.py
+++ spark-scripts/training/linear_regression.py
@@ -1,18 +1,21 @@
 """
-Script that trains a linear regression model per district using the preprocessing feature dataframe. 
-All rows until {split_date} are used for training. 
+Script that trains a linear regression model per district using the preprocessing feature dataframe.
+All rows until {split_date} are used for training.
 The models are stored in the specified {MODEL_FOLDER} using the following file name pattern: model_{LAT}_{LON}
 """
 
 import sys
-from datetime import datetime
+from pyspark.mllib.regression import LinearRegressionWithSGD
 
-from pyspark import SparkContext
-from pyspark import SparkConf
-from pyspark.sql import SQLContext
-from pyspark.mllib.regression import LabeledPoint, LinearRegressionWithSGD
-from pyspark.ml.feature import VectorAssembler
+from train import AbstractDistrictTrainer
 
+class LinearRegressionTrainer(AbstractDistrictTrainer):
+
+  def train(self, district):
+
+    return LinearRegressionWithSGD.train(self.get_train_data(district),
+                                         iterations=100,
+                                         step=0.00000001)
 
 # Get file paths from arguments
 if len(sys.argv) != 3:
@@ -21,37 +24,11 @@ if len(sys.argv) != 3:
 features_file = sys.argv[1]
 model_folder = sys.argv[2]
 
-# Configure Spark
-conf = (SparkConf().setAppName('train_linear_regression'))
-sc = SparkContext(conf=conf)
-sc.setLogLevel('WARN')
-sql_context = SQLContext(sc)
+trainer = LinearRegressionTrainer("train_linear_regression", features_file)
+trainer.initialize()
 
-# Read feature dataframe
-features_df = sql_context.read.parquet(features_file)
-
-# Split into train and test data
-split_date = datetime(2015, 1, 1)
-train_df = features_df.filter(features_df.Time < split_date)
-test_df = features_df.filter(features_df.Time > split_date)
-
-# Vectorize features
-feature_columns = [column for column in features_df.columns if column not in ['Time', 'Lat', 'Lon', 'Pickup_Count']]
-assembler = VectorAssembler(inputCols=feature_columns, outputCol='Features')
-vectorized_train_df = assembler.transform(train_df)
-
-# Create feature vector for each district
-def get_data_for_district(df, district):
-  lat, lon = district
-  return df.where((df.Lat == lat) & (df.Lon == lon))
-
-districts = features_df.select(features_df.Lat, features_df.Lon).distinct() \
-            .map(lambda row: (row.Lat, row.Lon)).collect()
-
-# Train model per district and save model
-vectorized_train_df = vectorized_train_df.cache()
-for lat, lon in districts:
-  points = get_data_for_district(vectorized_train_df, (lat, lon)).map(lambda row: LabeledPoint(row.Pickup_Count, row.Features))
-  if not points.isEmpty():
-    model = LinearRegressionWithSGD.train(points, iterations=100, step=0.00000001)
-    model.save(sc, '%s/model_%s_%s' % (model_folder, str(lat), str(lon)))
+for lat, lon, _ in trainer.districts_with_counts:
+  print("Training District: %f, %f" % (lat, lon))
+  model = trainer.train((lat, lon))
+  model.save(trainer.spark_context,
+             '%s/model_%s_%s' % (model_folder, str(lat), str(lon)))
---
+++ spark-scripts/training/train.py
@@ -0,0 +1,89 @@
+"""
+Script that trains a linear regression model per district using the preprocessing feature dataframe.
+All rows until {split_date} are used for training.
+The models are stored in the specified {MODEL_FOLDER} using the following file name pattern: model_{LAT}_{LON}
+"""
+
+import abc
+from datetime import datetime
+
+from pyspark import SparkContext
+from pyspark import SparkConf
+from pyspark.sql import SQLContext
+from pyspark.mllib.regression import LabeledPoint
+from pyspark.ml.feature import VectorAssembler
+
+class AbstractDistrictTrainer(object):
+
+  EXCLUDE_COLUMNS = ['Time', 'Lat', 'Lon', 'Pickup_Count']
+  DISTRICT_COUNT_THRESHOLD = 50
+
+  def __init__(self, app_name, features_file):
+
+    self.app_name = app_name
+    self.features_file = features_file
+
+
+  def initialize(self):
+    """Reads the dataset, initializes class members.
+
+    spark_context: The Spark Context
+    sql_context: The SQL Context
+    features_df: Original DataFrame as read from the features_file.
+    vectorized_train_df: A DataFrame with columns Lat, Lon, Pickup_Count and
+        vector column Features.
+    districts_with_counts: List of tuples (Lat, Lon, count) of all districts
+        that exceed DISTRICT_COUNT_THRESHOLD.
+    """
+
+    # Configure Spark
+    conf = (SparkConf().setAppName(self.app_name))
+    self.spark_context = SparkContext(conf=conf)
+    self.spark_context.setLogLevel('WARN')
+    self.sql_context = SQLContext(self.spark_context)
+
+    # Read feature dataframe
+    self.features_df = self.sql_context.read.parquet(self.features_file).cache()
+
+    # Split into train and test data
+    split_date = datetime(2015, 1, 1)
+    train_df = self.features_df.filter(self.features_df.Time < split_date)
+    test_df = self.features_df.filter(self.features_df.Time > split_date)
+
+    # Vectorize features
+    feature_columns = [column for column in self.features_df.columns
+                              if column not in self.EXCLUDE_COLUMNS]
+    assembler = VectorAssembler(inputCols=feature_columns, outputCol='Features')
+    self.vectorized_train_df = assembler.transform(train_df).cache()
+
+
+    self.districts_with_counts = self.features_df \
+                                 .groupBy([self.features_df.Lat, self.features_df.Lon]) \
+                                 .count()
+    self.districts_with_counts = self.districts_with_counts \
+        .where(self.districts_with_counts["count"] >= self.DISTRICT_COUNT_THRESHOLD) \
+        .map(lambda row: (row["Lat"], row["Lon"], row["count"])) \
+        .collect()
+
+
+  def get_data_for_district(self, df, district):
+    """Returns the subset of `df` that contains the district's data."""
+
+    lat, lon = district
+    return df.where((df.Lat == lat) & (df.Lon == lon))
+
+
+  def get_train_data(self, district):
+    """Returns an RDD of LabeledPoints."""
+
+    return self.get_data_for_district(self.vectorized_train_df, district) \
+           .map(lambda row: LabeledPoint(row.Pickup_Count, row.Features))
+
+
+  def train(self, district):
+    """Train the model.
+
+    Returns: A model instance with fit() and save() methods.
+    """
+
+    pass
