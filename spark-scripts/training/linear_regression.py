d19a5c17325896671ab371ebfb00186d4fac497f
Georg Wiese <georgwiese@gmail.com>
2016-06-22 19:30:06 +0200

    Train script: add districts file

--- spark-scripts/training/linear_regression.py
+++ spark-scripts/training/linear_regression.py
@@ -9,19 +9,19 @@ from pyspark.mllib.regression import LinearRegressionWithSGD
 
 from spark_application import create_spark_application
 from data_loader import DataLoader
+from reader import read_districts_file
 
 # Get file paths from arguments
-if len(sys.argv) != 3:
-  print "Usage: linear_regression.py FEATURES_FILE MODEL_FOLDER"
+if len(sys.argv) != 4:
+  print "Usage: linear_regression.py FEATURES_FILE MODEL_FOLDER DISTRICTS_FILE"
   sys.exit()
-features_file = sys.argv[1]
-model_folder = sys.argv[2]
+features_file, model_folder, districts_file = sys.argv[1:]
 
 spark_context, sql_context = create_spark_application("train_linear_regression")
 data_loader = DataLoader(spark_context, sql_context, features_file)
 data_loader.initialize()
 
-for lat, lon, _ in data_loader.districts_with_counts:
+for lat, lon in read_districts_file(districts_file):
   print("Training District: %f, %f" % (lat, lon))
   model = LinearRegressionWithSGD.train(data_loader.get_train_data((lat, lon)),
                                         iterations=100,
