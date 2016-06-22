59647178c7a70b62286a4e946c3593c1cbd4a516
Georg Wiese <georgwiese@gmail.com>
2016-06-22 20:38:02 +0200

    Write evaluation result csv

--- spark-scripts/evaluation/evaluate.py
+++ spark-scripts/evaluation/evaluate.py
@@ -15,18 +15,20 @@ MODEL_TYPE_TO_CLASS = {
 }
 
 # Get file paths from arguments
-if len(sys.argv) != 5:
-  print "Usage: linear_regression.py FEATURES_FILE MODEL_FOLDER MODEL_TYPE DISTRICTS_FILE"
+if len(sys.argv) != 6:
+  print "Usage: linear_regression.py FEATURES_FILE MODEL_FOLDER MODEL_TYPE DISTRICTS_FILE RESULT_CSV"
   print "  where model type one of: %s" % str(MODEL_TYPE_TO_CLASS.keys())
   print "  and the districts file is a text file with lines \"<lat>, <lon>\"."
   sys.exit()
-features_file, model_folder, model_type, districts_file = sys.argv[1:]
+features_file, model_folder, model_type, districts_file, result_csv = sys.argv[1:]
 
 ModelClass = MODEL_TYPE_TO_CLASS[model_type]
 
 spark_context, sql_context = create_spark_application("evaluate_linear_regression")
 data_loader = DataLoader(spark_context, sql_context, features_file)
 data_loader.initialize()
+
+results = []
 
 for district in read_districts_file(districts_file):
   print("Evaluating district: %s" % str(district))
@@ -40,6 +42,17 @@ for district in read_districts_file(districts_file):
                                            point.label))
 
   metrics = RegressionMetrics(predictions_labels)
+  mse, rmse = metrics.meanSquaredError, metrics.rootMeanSquaredError
+  results.append((district, mse, rmse))
 
-  print("MSE = %s" % metrics.meanSquaredError)
-  print("RMSE = %s" % metrics.rootMeanSquaredError)
+  print("MSE = %s" % mse)
+  print("RMSE = %s" % rmse)
+
+# Write Result CSV
+with open(result_csv, "w") as f:
+  f.write("lat, lon, mse, rmse\n")
+
+  for district, mse, rmse in results:
+    lat, lon = district
+    f.write("%s, %s, %f, %f" % (str(lat), str(lon), mse, rmse))
+
