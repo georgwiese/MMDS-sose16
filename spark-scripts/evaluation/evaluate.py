0fb46141f567ef1944dc50fd6d600a5549556e6e
Georg Wiese <georgwiese@gmail.com>
2016-06-23 16:34:44 +0200

    Evaluate script: provide result path

--- spark-scripts/evaluation/evaluate.py
+++ spark-scripts/evaluation/evaluate.py
@@ -3,6 +3,7 @@ Script that evaluates a given model.
 """
 
 import sys
+import os
 from pyspark.mllib.regression import LinearRegressionModel
 from pyspark.mllib.evaluation import RegressionMetrics
 
@@ -16,11 +17,11 @@ MODEL_TYPE_TO_CLASS = {
 
 # Get file paths from arguments
 if len(sys.argv) != 6:
-  print "Usage: linear_regression.py FEATURES_FILE MODEL_FOLDER MODEL_TYPE DISTRICTS_FILE RESULT_CSV"
+  print "Usage: linear_regression.py FEATURES_FILE MODEL_FOLDER MODEL_TYPE DISTRICTS_FILE RESULT_PATH"
   print "  where model type one of: %s" % str(MODEL_TYPE_TO_CLASS.keys())
   print "  and the districts file is a text file with lines \"<lat>, <lon>\"."
   sys.exit()
-features_file, model_folder, model_type, districts_file, result_csv = sys.argv[1:]
+features_file, model_folder, model_type, districts_file, result_path = sys.argv[1:]
 
 ModelClass = MODEL_TYPE_TO_CLASS[model_type]
 
@@ -39,7 +40,8 @@ for district in read_districts_file(districts_file):
 
   predictions_labels = data_loader.get_test_data(district) \
                        .map(lambda point: (float(model.predict(point.features)),
-                                           point.label))
+                                           point.label)).cache()
+  print(predictions_labels.take(10))
 
   metrics = RegressionMetrics(predictions_labels)
   mse, rmse = metrics.meanSquaredError, metrics.rootMeanSquaredError
@@ -49,10 +51,13 @@ for district in read_districts_file(districts_file):
   print("RMSE = %s" % rmse)
 
 # Write Result CSV
-with open(result_csv, "w") as f:
+model_name = model_folder.split("/")[-1]
+filename = os.path.join(result_path, "%s_result.csv" % model_name)
+
+with open(filename, "w") as f:
   f.write("lat, lon, mse, rmse\n")
 
   for district, mse, rmse in results:
     lat, lon = district
-    f.write("%s, %s, %f, %f" % (str(lat), str(lon), mse, rmse))
+    f.write("%s, %s, %f, %f\n" % (str(lat), str(lon), mse, rmse))
 
