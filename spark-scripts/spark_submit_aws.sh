d3e3fd798fcfe54f4dd50d79ea791ae49ac682a7
root <root@ip-172-31-33-10.eu-west-1.compute.internal>
2016-07-10 12:09:15 +0000

    Add spark_submit_aws

---
+++ spark-scripts/spark_submit_aws.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+#
+# Runs spark-submit with our default args and the python file that is passed to
+# this script. Stores the output in a timestamped logfile.
+
+if [  $# -lt 1 ]
+then
+  echo "Usage: $0 <spark python script> [optional arguments]"
+  exit 1
+fi
+
+py_file=$1
+shift
+script_args=$@
+
+filename=$(basename "$py_file")
+scriptname="${filename%.*}"
+timestamp=$(date +"%m_%d-%H_%M_%S")
+logfile="${scriptname}-${timestamp}.log"
+
+# Default spark arguments
+spark_packages="com.databricks:spark-csv_2.11:1.4.0"
+spark_memory="4G"
+dns_name=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
+spark_master="spark://${dns_name}:7077"
+
+echo $spark_master
+
+mkdir -p logs
+
+echo "$py_file $script_args" > logs/$logfile
+
+/root/spark/bin/spark-submit\
+  --master $spark_master\
+  --executor-memory $spark_memory\
+  --driver-memory $spark_memory\
+  --packages $spark_packages\
+  $py_file $script_args &>> logs/$logfile
--- spark-scripts/train_evaluate.sh
+++ spark-scripts/train_evaluate.sh
@@ -1,6 +1,6 @@
-if [  $# -lt 5 ]
+if [  $# -lt 6 ]
 then
-  echo "Usage: $0 <model type> <feature file> <model folder> <districs file> <result path>"
+  echo "Usage: $0 <model type> <feature file> <model folder> <districs file> <result path> <(aws|tenem)>"
   exit 1
 fi
 
@@ -9,6 +9,7 @@ features_file=$2
 model_folder=$3
 districts_file=$4
 result_path=$5
+cluster=$6
 
 if [ "$model_type" == "linear" ]
 then
@@ -24,7 +25,7 @@ fi
 py_evaluation_file="evaluation/evaluate.py"
 
 echo "Train model"
-./spark_submit_tenem.sh $py_training_file $features_file $model_folder $districts_file
+./spark_submit_${cluster}.sh $py_training_file $features_file $model_folder $districts_file
 
 echo "Evaluate model"
-./spark_submit_tenem.sh $py_evaluation_file $features_file $model_folder $model_type $districts_file $result_path
+./spark_submit_${cluster}.sh $py_evaluation_file $features_file $model_folder $model_type $districts_file $result_path
